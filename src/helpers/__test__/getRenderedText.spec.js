760010d90b3eabd45eb6c70dae23fd9dad662b58
Albert Nevado <albert.nevado@gmail.com>
2021-11-02 10:25:05 +0100

    Setup test files for helpers

---
+++ src/helpers/__test__/getRenderedText.spec.js
@@ -0,0 +1,8 @@
+import getRenderedText from '../getRenderedText';
+
+describe('getRenderedText helper', () => {
+  it.todo('returns the text correctly if it is sent as a string');
+  it.todo(
+    'returns the rendered result of the method provided if it is sent as a method'
+  );
+});
---
+++ src/helpers/__test__/getSecurityKeysStatus.spec.js
@@ -0,0 +1,7 @@
+import getSecurityKeysStatus from '../getSecurityKeysStatus';
+
+describe('getSecurityKeysStatus helper', () => {
+  it.todo('returns succeeded and failed hints correctly');
+  it.todo('returns the expected result if conditions are RegExp');
+  it.todo('returns the expected result if conditions are boolean methods');
+});
