1325f4bc954d57bc08c10480e98667b015d5cd6d
Albert Nevado <albert.nevado@gmail.com>
2021-11-03 15:25:19 +0100

    Complete testing for helpers

--- src/helpers/__test__/getRenderedText.spec.js
+++ src/helpers/__test__/getRenderedText.spec.js
@@ -1,8 +1,11 @@
 import getRenderedText from '../getRenderedText';
 
 describe('getRenderedText helper', () => {
-  it.todo('returns the text correctly if it is sent as a string');
-  it.todo(
-    'returns the rendered result of the method provided if it is sent as a method'
-  );
+  it('returns the text correctly if it is sent as a string', () => {
+    expect(getRenderedText('test')).toEqual('test');
+  });
+
+  it('returns the rendered result of the method provided if it is sent as a method', () => {
+    expect(getRenderedText(() => 'method test')).toEqual('method test');
+  });
 });
